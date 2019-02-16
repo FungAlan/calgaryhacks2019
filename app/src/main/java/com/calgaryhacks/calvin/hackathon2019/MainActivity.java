c10eddf814a71acb10c27a46b1271d003c3a6c43
calvin <calvin.bochulak@gmail.com>
2019-02-16 16:18:52 -0700

    Add bottom navigation to app. Including Home, Reports, and Profile

--- app/src/main/java/com/calgaryhacks/calvin/hackathon2019/MainActivity.java
+++ app/src/main/java/com/calgaryhacks/calvin/hackathon2019/MainActivity.java
@@ -1,7 +1,11 @@
 package com.calgaryhacks.calvin.hackathon2019;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomNavigationView.OnNavigationItemSelectedListener;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigation = findViewById(R.id.navigationView);
+        bottomNavigation.setOnNavigationItemSelectedListener(listener);
     }
+
+    private OnNavigationItemSelectedListener listener = new OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            switch(menuItem.getItemId()) {
+                case R.id.navigation_home:
+                case R.id.navigation_reports:
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    };
+
 }
