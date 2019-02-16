b9e18c8d046f022f4fe35b617c34ee4a581d3dc7
Bebo <rfiker@ucalgary.ca>
2019-02-16 15:01:22 -0700

    Firebase Realtime Database is set up now.

--- app/src/main/java/com/calgaryhacks/calvin/hackathon2019/MainActivity.java
+++ app/src/main/java/com/calgaryhacks/calvin/hackathon2019/MainActivity.java
@@ -3,11 +3,14 @@ package com.calgaryhacks.calvin.hackathon2019;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.FirebaseDatabase;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        FirebaseDatabase.getInstance().getReference().setValue("Board is green!");
     }
 }
