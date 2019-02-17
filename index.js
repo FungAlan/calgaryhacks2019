009851c6e05a4a5474f831b432c204c00ee8ce1c
Martin Tran <martin.hua.tran@gmail.com>
2019-02-16 22:19:15 -0700

    added intent handlers for road construction and traffic accidents

--- index.js
+++ index.js
@@ -12,26 +12,39 @@ const app = dialogflow({debug: true});
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(app);
 
 app.intent('Default Welcome Intent', (conv) => {
-	conv.ask(new Permission({
-		context: 'To know where incident reports took place',
-		permissions: ['NAME', 'DEVICE_PRECISE_LOCATION']
-	}));
+	if (!conv.user.storage.returning) {
+		conv.ask(new Permission({
+			context: 'To know where incident reports took place',
+			permissions: ['NAME', 'DEVICE_PRECISE_LOCATION']
+		}));
+	} else {
+		conv.ask(new Permission({
+			context: 'Say yes',
+			permissions: 'DEVICE_PRECISE_LOCATION'
+		}));
+	}
 });
 
 app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
 	if (!permissionGranted) {
-		conv.ask('Ok, no worries. When making reports, you will need to specify locations');
+		conv.close('Sorry, we need location data to pinpoint any reports.');
 	} else {
-		conv.user.storage.location = conv.device.location;
+		conv.user.storage.returning = true;
 		conv.ask('Thank you, what would you like to report?');
 	}
 });
 
 app.intent('Icy Road', (conv) => {
-	if (conv.user.storage.location) {
-		conv.close(`Sent a report of icy roads at (${conv.user.storage.location.coordinates.latitude}, ${conv.user.storage.location.coordinates.longitude})`);
-	} else {
-		conv.close('Did not have permission to access device location');
-	}
+	const loc = conv.device.location;
+	conv.close(`Sent a report of icy roads at (${loc.coordinates.latitude}, ${loc.coordinates.longitude})`);
 });
 
+app.intent('Car Accident', (conv) => {
+	const loc = conv.device.location;
+	conv.close(`Alright, the report of a traffic accident at (${loc.coordinates.latitude}, ${loc.coordinates.longitude}) is on its way.`); 
+});
+
+app.intent('Road Construction', (conv) => {
+	const loc = conv.device.location;
+	conv.close(`Got it, report of construction at (${loc.coordinates.latitude}, ${loc.coordinates.longitude}) has been noted.`); 
+});
